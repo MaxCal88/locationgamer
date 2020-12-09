f84c1ac1980480146c2f7c3758c96a9b6e52e80e
Maximilian Zellner <maximilianzellner@MacBook-Pro.local>
2020-12-08 19:46:02 -0800

    Adding function documentation

--- R/djikstra.R
+++ R/djikstra.R
@@ -1,5 +1,28 @@
-# Finds the shortest path through the connected network using Djikstra's algorithm
-# Output is the shortest path and the shortest distance
+#' @title Shortest path through network using Djikstra's algorithm
+#'
+#' @description This function finds the shortest path from a starting node to an end node in a network
+#' specified by an edge matrix and vertex coordinates. Position i,j of the edge matrix is one if there
+#' is an edge between the ith and jth vertex, zero otherwise. The function returns the path NA with length
+#' infinity if the network is disconnected, i.e. if no shortest path can be found.
+#'
+#' @param edgeMatrix A square matrix consisting of zeros and ones. Has to be zero on the diagonals
+#' @param coordMatrix A data frame containing the x and y coordinates of each network vertex
+#' @param initialNode A number corresponding to the start node/ vertex
+#' @param endNode A number corresponding to the end node/ vertex
+#' @param nNodes The number of vertices/ nodes in the network
+#' @return A list consisting of a vector with the vertices/ nodes visited by the shortest path and the length of the shortest path.
+#' @examples
+#' initialNode <- 1
+#' endNode <- 4
+#' nNodes <- 4
+#' edgeMatrix <- matrix(0, nrow = 4, ncol = 4)
+#' edgeMatrix[,1] <- c(0,1,0,0)
+#' edgeMatrix[,2] <- c(1,0,1,1)
+#' edgeMatrix[,3] <- c(0,1,0,0)
+#' edgeMatrix[,4] <- c(0,1,0,0)
+#' coordMatrix <- matrix(c(0,10,15,20,30,30,15,15),ncol = 2)
+#' djikstra(edgeMatrix, coordMatrix, initialNode, endNode, nNodes)
+#'
 
 djikstra <- function(edgeMatrix, coordMatrix, initialNode, endNode, nNodes){
   unvisitedNodes <- as.data.frame(matrix(NA, nrow = nNodes, ncol = 4))
--- R/plotPrim.R
+++ R/plotPrim.R
@@ -1,10 +1,20 @@
-# Plots the minimum spanning tree derived by function primDistance
-plotPrim <- function(shortestPath, coordMat){
+#' @title Plotting minimum spanning tree connecting all vertices
+#'
+#' @param minimumSp A data frame in which each row corresponds to an edge between two numbered vertices. Use function primDistance
+#' to obtain minimum spanning tree using Prim's algorithm.
+#' @param coordMat A matrix containing all the x and y coordinates of the network vertices
+#'
+#' @examples
+#' minimumSp <- matrix(c(1,4,4,3,2,3),ncol = 2)
+#' coordMatrix <- matrix(c(0,10,15,20,30,30,15,15),ncol = 2)
+#' plotPrim(minimumSp, coordMatrix)
+
+plotPrim <- function(minimumSp, coordMat){
   minAx <- min(union(coordMat[,1], coordMat[,2]))
   maxAx <- max(union(coordMat[,1], coordMat[,2]))
   par(pty = "s")
   plot(coordMat[,1], coordMat[,2], xlim = c(minAx,maxAx), ylim = c(minAx,maxAx), xlab = "X", ylab = "Y", main = "Prim - Minimum spanning tree")
-  for (i in 1:dim(shortestPath)[1]){
-    segments(coordMat[shortestPath[i,1],1],coordMat[shortestPath[i,1],2],coordMat[shortestPath[i,2],1],coordMat[shortestPath[i,2],2])
+  for (i in 1:dim(minimumSp)[1]){
+    segments(coordMat[minimumSp[i,1],1],coordMat[minimumSp[i,1],2],coordMat[minimumSp[i,2],1],coordMat[minimumSp[i,2],2])
   }
 }
